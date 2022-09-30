aa3d88b758890994abc2a1c9dd4e6b59afb0b8ff
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-09-30 13:48:41 -0300

    Category

--- src/main/java/com/webservices/project/config/TestConfig.java
+++ src/main/java/com/webservices/project/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.webservices.project.config;
 
+import com.webservices.project.entities.Category;
 import com.webservices.project.entities.Order;
 import com.webservices.project.entities.User;
 import com.webservices.project.entities.enums.OrderStatus;
+import com.webservices.project.repositories.CategoryRepository;
 import com.webservices.project.repositories.OrderRepository;
 import com.webservices.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -33,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
---
+++ src/main/java/com/webservices/project/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.webservices.project.repositories;
+
+import com.webservices.project.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
