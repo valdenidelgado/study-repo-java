e280433e69092cfcbad7d8d551e69db84bc3ec3c
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-09-30 22:26:33 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/webservices/project/config/TestConfig.java
+++ src/main/java/com/webservices/project/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.webservices.project.config;
 
-import com.webservices.project.entities.Category;
-import com.webservices.project.entities.Order;
-import com.webservices.project.entities.Product;
-import com.webservices.project.entities.User;
+import com.webservices.project.entities.*;
 import com.webservices.project.entities.enums.OrderStatus;
-import com.webservices.project.repositories.CategoryRepository;
-import com.webservices.project.repositories.OrderRepository;
-import com.webservices.project.repositories.ProductRepository;
-import com.webservices.project.repositories.UserRepository;
+import com.webservices.project.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,5 +65,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- src/main/java/com/webservices/project/entities/Order.java
+++ src/main/java/com/webservices/project/entities/Order.java
@@ -5,7 +5,9 @@ import com.webservices.project.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +24,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -64,6 +69,10 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
