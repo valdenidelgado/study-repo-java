eb94841e3e415685aa17806b0becfe6fadb32b96
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-03 02:30:35 -0300

    Exception handling - findById

---
+++ src/main/java/com/webservices/project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.webservices.project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
