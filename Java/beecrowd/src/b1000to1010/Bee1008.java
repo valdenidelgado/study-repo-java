63229c91e56a083daf629e686e3b4fdfc94a9c00
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-25 16:58:48 -0300

    fix bee1006 correctly

--- Java/beecrowd/src/b1000to1010/Bee1008.java
+++ Java/beecrowd/src/b1000to1010/Bee1008.java
@@ -2,16 +2,16 @@ package b1000to1010;
 
 import java.util.Scanner;
 
-public class Bee1006 {
+public class Bee1008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         float a = sc.nextFloat();
         float b = sc.nextFloat();
-        float c = sc.nextFloat();
 
-        double average = (a * 2 + b * 3 + c * 5) / 10;
+        double average = (a * 3.5 + b * 7.5) / 11;
 
-        System.out.printf("MEDIA = %.1f\n", average);
+        System.out.printf("MEDIA = %.5f\n", average);
         sc.close();
     }
 }
---
+++ Java/beecrowd/src/b1000to1010/Bee1009.java
@@ -0,0 +1,18 @@
+package b1000to1010;
+
+import java.util.Scanner;
+
+public class Bee1006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        double salary = sc.nextDouble();
+        double sell = sc.nextDouble();
+
+        double total = salary + sell * 0.15;
+
+        System.out.printf("TOTAL = R$ %.2f\n", total);
+        sc.close();
+    }
+}
