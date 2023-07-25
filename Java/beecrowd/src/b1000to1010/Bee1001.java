051bfbe22da816bd1675d11b803f5a43e4cd5954
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-25 16:07:02 -0300

    beecrowd challenge 1001

---
+++ Java/beecrowd/src/b1000to1010/Bee1001.java
@@ -0,0 +1,15 @@
+package b1000to1010;
+
+import java.util.Scanner;
+
+public class Bee1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("X = %d\n", a + b);
+        sc.close();
+    }
+}
