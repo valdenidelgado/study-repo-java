a156078c794f49efc87135381b428b69c9542a84
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-26 17:13:59 -0300

    beecrowd challenge 1016

---
+++ Java/beecrowd/src/b1010to1020/Bee1016.java
@@ -0,0 +1,17 @@
+package b1010to1020;
+
+import java.util.Scanner;
+
+public class Bee1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int distance = a * 2;
+
+        System.out.printf("%d minutos\n", distance);
+
+        sc.close();
+    }
+}
